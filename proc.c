0c38d093120fb6307f35acf35eeabe526a5b5b85
mmsamiei <mohmahsamiei@gmail.com>
2017-01-14 19:35:55 +0330

    implementation of 2nd policy

--- proc.c
+++ proc.c
@@ -524,9 +524,10 @@ wakeup1(void *chan)
   struct proc *p;
 
   for(p = ptable.proc; p < &ptable.proc[NPROC]; p++)
-    if(p->state == SLEEPING && p->chan == chan)
+    if(p->state == SLEEPING && p->chan == chan){
       p->state = RUNNABLE;
-      insert(p);
+        insert(p);
+    }
 }
 
 // Wake up all processes sleeping on chan.
