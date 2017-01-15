bcffc9ed1cb3a4fd4f3b5cb7d9e8f0f1eaa1f8bf
Iman Tabrizian <Tabrizian@outlook.com>
2017-01-15 22:11:03 +0330

    Nothing

--- user.h
+++ user.h
@@ -23,7 +23,7 @@ int getpid(void);
 char* sbrk(int);
 int sleep(int);
 int uptime(void);
-int wait2(int * ,int*);
+int wait2(int*, int*);
 // ulib.c
 int stat(char*, struct stat*);
 char* strcpy(char*, char*);
