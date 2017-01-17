7a064bd78faa2888930fad06d09c5f54cc38860e
Iman Tabrizian <Tabrizian@outlook.com>
2017-01-17 15:09:20 +0330

    Adds two new system calls

--- syscall.c
+++ syscall.c
@@ -8,8 +8,7 @@
 #include "syscall.h"
 
 // User code makes a system call with INT T_SYSCALL.
-// System call number in %eax.
-// Arguments on the stack, from the user call to the C
+// System call number in %eax.  // Arguments on the stack, from the user call to the C
 // library system call function. The saved user %esp points
 // to a saved program counter, and then the first argument.
 
@@ -100,6 +99,9 @@ extern int sys_wait(void);
 extern int sys_write(void);
 extern int sys_uptime(void);
 extern int sys_wait2(void);
+extern int sys_nice(void);
+extern int sys_sth(void);
+
 
 static int (*syscalls[])(void) = {
 [SYS_fork]    sys_fork,
@@ -125,6 +127,8 @@ static int (*syscalls[])(void) = {
 [SYS_mkdir]   sys_mkdir,
 [SYS_close]   sys_close,
 [SYS_wait2]   sys_wait2,
+[SYS_nice]    sys_nice,
+[SYS_sth]    sys_sth,
 };
 
 void
